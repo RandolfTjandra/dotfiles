39bb7e5e4520f3bccac9a6d3ddcccf32b59d33e1
Randolf Tjandra <randolftjandra@gmail.com>
2022-10-20 09:55:20 -0700

    Add sketchybar battery plugin

---
+++ sketchybar/plugins/battery.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
+CHARGING=$(pmset -g batt | grep 'AC Power')
+
+if [ $PERCENTAGE = "" ]; then
+	exit 0
+fi
+
+case ${PERCENTAGE} in
+9[0-9] | 100)
+	ICON=""
+	;;
+[6-8][0-9])
+	ICON=""
+	;;
+[3-5][0-9])
+	ICON=""
+	;;
+[1-2][0-9])
+	ICON=""
+	;;
+*) ICON="" ;;
+esac
+
+if [[ $CHARGING != "" ]]; then
+	ICON=""
+fi
+
+# The item invoking this script (name $NAME) will get its icon and label
+# updated with the current battery status
+sketchybar --set $NAME icon="$ICON" label="${PERCENTAGE}%"
+
+#DETAILS="$(pmset -g batt)"
+## SOURCE="$(echo "$DETAILS" | grep -o "\'.*\'" | sed "s/'//g")"
+#PERCENT="$(echo "$DETAILS" | grep -o "\d*\%")"
+#REMAINING="$(echo "$DETAILS" | grep -o " \d*:\d* ")"
+
+#sketchybar --set $NAME icon="$PERCENT" label="$REMAINING"
