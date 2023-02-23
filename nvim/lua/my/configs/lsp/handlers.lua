87eba85a681ffd2d7706d13e34ff70698ab12372
Randolf Tjandra <randolftjandra@gmail.com>
2023-02-23 08:57:56 -0800

    Clean up handlers.lua

--- nvim/lua/my/configs/lsp/handlers.lua
+++ nvim/lua/my/configs/lsp/handlers.lua
@@ -44,12 +44,8 @@ M.on_attach = function(client, bufnr)
 end
 
 local capabilities
-
--- capabilities = vim.lsp.protocol.make_client_capabilities()
--- capabilities = require("cmp_nvim_lsp").default_capailities()
 local cap = vim.lsp.protocol.make_client_capabilities()
 local capabilities = require("cmp_nvim_lsp").default_capabilities(cap)
-
 M.capabilities = capabilities
 
 return M
